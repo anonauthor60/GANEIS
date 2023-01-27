5cee699e3f26f99ca901d8a1400055eedb6a2030
anonauthor60 <122845715+anonauthor60@users.noreply.github.com>
2023-01-27 09:45:43 +0900

    Update train_sim_net.py

--- train_sim_net.py
+++ train_sim_net.py
@@ -5,6 +5,6 @@ from ganeis.mol_sim import train_sim_net
 
 
 elem_attrs = load_elem_attrs('res/matscholar-embedding.json')
-dataset = load_dataset(path_dataset='../../data/chem_data/qm9_max6.xlsx', elem_attrs=elem_attrs, idx_struct=0)
-sim_net = train_sim_net(dataset, n_epochs=1)
+dataset = load_dataset(path_dataset='res/qm9_max6.xlsx', elem_attrs=elem_attrs, idx_struct=0)
+sim_net = train_sim_net(dataset)
 torch.save(sim_net.state_dict(), 'res/mol_sim_net.pt')
